538c6758353337dabdb7a43e7e9ffe0cfbcf20a1
PothireddyNavitha <pothireddynavitha35@gmail.com>
2026-02-19 11:00:05 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,10 @@
+public class Cal {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		int add=a+b;
+		int sub=a-b;
+		System.out.println("The sum is:"+add);
+		System.out.println("The Difference is:"+sub);
+	}
+}
